18d85a1148de255bfa530dc4e70e6a2b17241b2f
jimin <minlog.0430@gmail.coom>
2024-07-23 23:04:54 +0900

    로그인 관련 페이지 작업

--- resource/js/common.js
+++ resource/js/common.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
 
-	// 모든 셀렉트 스타일 추가
-	$('select').selectric();
+	// 헤더 푸터 
+	function includeHTML() {
+		let elements = document.querySelectorAll('[data-include]');
+		elements.forEach(element => {
+			let file = element.getAttribute('data-include');
+			fetch(file)
+				.then(response => response.text())
+				.then(data => {
+					element.innerHTML = data;
+
+					// 모든 셀렉트 스타일 추가
+					$('select').selectric();
+
+				});
+		});
+	}
+	window.onload = includeHTML;
 
 });
